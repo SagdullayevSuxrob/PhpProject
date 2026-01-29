feb00d96aaa830398b85f93f28e7216c86b671d0
Suxrob Sagdullayev <suxrobsagdullayev5@gmail.com>
2026-01-29 21:19:29 +0500

    PHP PROJECT

---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<?php 
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/style.css">
+    <title><?= $title ?? "PHP Blog"; ?></title>
+</head>
+
+<body>
+
+    <header>
+       <?php require('navbar.php')?>
+    </header>
+
+    <main>
---
+++ post.php
@@ -0,0 +1,26 @@
+<?php
+$title = "Post";
+require('includes/header.php');
+
+require('database.php');
+
+$id = $_GET['id'];
+
+$statement = $pdo->prepare("SELECT * FROM posts WHERE id = ? ");
+$statement->execute([$id]);
+$post = $statement->fetch();
+
+?>
+
+
+<div class="container mt-5 py-3">
+    <h3><?= $post['id'] ?></h3>
+    <h1><?= $post['title'] ?></h1>
+    <p class="fs-5 col-md-8"><?= $post['body'] ?></p>
+    <p><small><?= $post['created_at'] ?></small></p>    
+
+    <hr class="col-3 col-md-2 mb-5">
+</div>
+
+
+<?php require('includes/footer.php') ?>
